34ff34ca34b95f8fb1ee687c8cafbcd4663bc273
Geoff Soord <Geoff-Soord@SoftwareImaging.local>
2018-09-13 16:15:53 +0100

    On the road to using the new public API v1.0

--- genius.py
+++ genius.py
@@ -13,7 +13,9 @@ from homeassistant.components.climate import (
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE, CONF_HOST, CONF_API_KEY
 import homeassistant.helpers.config_validation as cv
 
-from .utils import GeniusUtility
+import json
+import requests
+
 
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE | SUPPORT_ON_OFF
 
@@ -22,53 +24,56 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
 })
 
+HG_URL = "https://my.geniushub.co.uk/v1"
+
+
+def getjson(host, api_key, identifier):
+    """ gets the json from the supplied zone identifier """
+    url = host + identifier
+    try:
+        headers = {'Authorization': 'Bearer ' + api_key}
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            return json.loads(response.text)
+
+    except Exception as ex:
+        print("Failed requests in getjsonfromhttp")
+        print(ex)
+        return None
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Demo climate devices."""
     host = config.get(CONF_HOST)
     api_key = config.get(CONF_API_KEY)
 
-    # To switch to read JSON from file add True as the last parameter to the
-    # constructor. This is to aid debugging only
-    genius_utility = GeniusUtility(host, api_key)
-
-    whole_house = genius_utility.GETJSON(0)
-    zone_list = genius_utility.getzonelist(whole_house)
+    zone_list = getjson(HG_URL, api_key, '/zones')
 
     for zone in zone_list:
-        devices = zone_list[zone]['zone']
-        mode = zone_list[zone]['iMode']
-        key = zone_list[zone]['key']
-        current_temperature = None
-        target_temperature = None
+        if 'temperature' in zone:
+            current_temperature = zone['temperature']
+            target_temperature = zone['setpoint']
+            name = zone['name']
+            id = zone['id']
+            mode = zone['mode']
 
-        for device in devices.items():
-            device_id, device_raw = device
-            device_type = device_raw['type']
-            if device_type == 'Sensor':
-                current_temperature = device_raw['TEMPERATURE']
-            elif device_type == 'Radiator valve':
-                target_temperature = device_raw['HEATING_1']
-
-        if current_temperature and target_temperature:
-            add_devices([GeniusDevice(genius_utility,
-                                      zone, key, device_id, mode, target_temperature, current_temperature)])
+            add_devices([GeniusDevice(name, id, mode,
+                                      target_temperature, current_temperature)])
 
 
 class GeniusDevice(ClimateDevice):
     """Representation of a demo climate device."""
 
-    def __init__(self, genius_utility, name, key, device_id, mode, target_temperature, current_temperature):
+    def __init__(self, name, device_id, mode, target_temperature, current_temperature):
         """Initialize the climate device."""
-        self._genius_utility = genius_utility
         self._name = name
         self._support_flags = SUPPORT_FLAGS
         self._target_temperature = target_temperature
         self._unit_of_measurement = TEMP_CELSIUS
         self._current_temperature = current_temperature
         self._device_id = device_id
-        self._key = key
-        self._current_operation = genius_utility.GET_MODE(mode)
+        self._current_operation = mode
 
         if self._current_operation == "off":
             self._on = False
@@ -131,9 +136,9 @@ class GeniusDevice(ClimateDevice):
 
     def update(self):
         """Get the latest date."""
-        self._current_temperature, self._target_temperature, mode = self._genius_utility.GET_TEMPERATURE(
+        ''' self._current_temperature, self._target_temperature, mode = self._genius_utility.GET_TEMPERATURE(
             self._key)
-        self._current_operation = self._genius_utility.GET_MODE(mode)
+        self._current_operation = self._genius_utility.GET_MODE(mode) '''
         self.schedule_update_ha_state()
 
     def turn_on(self):
